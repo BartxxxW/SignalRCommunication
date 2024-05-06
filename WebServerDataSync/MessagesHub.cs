d1b37819aa74486fd013d770fbaa794d38fb9c86
Bartosz Wojnarowski <bartosz.wojnarowski@o2.pl>
2024-05-06 13:39:08 +0200

    base SetUp

---
+++ WebServerDataSync/MessagesHub.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Owin.Security.Infrastructure;
+
+namespace WebServerDataSync
+{
+    public class MessagesHub:Hub
+    {
+        public const string ReceiveMessage = "ReceiveMessage";
+        public async Task Send( string message)
+        {
+
+
+            await Clients.Others.SendAsync(ReceiveMessage, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var clients = Clients;
+            var context = Context;
+
+            return Task.CompletedTask;
+        }
+        //sendsServerWebClietns
+
+        //SendDesktopClients
+    }
+}
---
+++ WebServerDataSync/wwwroot/js/messageApp.js
@@ -0,0 +1,59 @@
+﻿
+(async function () {
+    const btnSend = document.getElementById("buttonSend");
+    const message = document.getElementById("messageToSend");
+    const receivedMsg = document.getElementById("receivedMsg");
+ 
+
+
+
+
+    $(btnSend).click(async () => {
+
+        const msg = $(message).val();
+
+        if (!msg || msg == '') { return }
+
+        try {
+            await connection.invoke("Send", msg);
+        } catch (err) {
+            console.error(err);
+        }
+
+    })
+
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/messageHub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    async function start() {
+        try {
+            await connection.start();
+            console.log("SignalR Connected.");
+        } catch (err) {
+            console.log(err);
+            setTimeout(start, 5000);
+        }
+    };
+
+
+
+
+    connection.onclose(async () => {
+        await start();
+    });
+
+
+    connection.on("ReceiveMessage", (message) => {
+        alert(message);
+        $("#receivedMsg").text(message);
+
+    });
+
+    // Start the connection.
+    start();
+})();
+
+
+
